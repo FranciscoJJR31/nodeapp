a35aaded99f79040c118b1102eef6ab92454c350
FranciscoJJR31 <javier_3197@hotmail.com>
2024-11-28 21:41:53 -0500

    New

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const Fechaformat =  'YYYY-MM-DD';
   //const WebSocket = require("ws").Server; 
   const cors = require('cors');
  
-
-
-  
 const pool = new Pool({
   user: 'appuser',
   //host: 'postgres',
